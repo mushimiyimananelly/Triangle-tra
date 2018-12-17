94cb66c81a73cd23ff76f99a9bc8f33bc1310259
first-name second-name <email@example.com>
2018-12-17 15:34:31 +0200

    add function

---
+++ scripts.js
@@ -0,0 +1,18 @@
+
+function getResults(){
+    var side1= parseFloat(document.getElementById('side1')).value; 
+    var side2= parseFloat(document.getElementById('side2')).value; 
+    var side3= parseFloat(document.getElementById('side2')).value; 
+    if(side1 === side2 && side1 === side1 && side2 === side3){
+    alert("this is an Equilateral");
+    }
+    else if(side1 === side2 || side1 === side3 || side2 === side3){
+    alert("Isosceles");
+    }
+    else if((side1 + side2) <= side3 || (side1 + side3) <= side2 || (side2 + side3) <= side1) {
+    alert("Not a definite triangle");
+    }
+    else{
+    alert("Scalene");
+    }
+    }
