ec1d4a25f0665d1914f3ee35f06c31146a5a045e
first-name second-name <email@example.com>
2018-12-17 16:57:04 +0200

    add changes

--- scripts.js
+++ scripts.js
@@ -1,18 +1,21 @@
 
 function getResults(){
-    var side1= parseFloat(document.getElementById('side1')).value; 
-    var side2= parseFloat(document.getElementById('side2')).value; 
-    var side3= parseFloat(document.getElementById('side2')).value; 
-    if(side1 === side2 && side1 === side1 && side2 === side3){
+    var side1= parseFloat(document.getElementById('side1').value); 
+    var side2= parseFloat(document.getElementById('side2').value); 
+    var side3= parseFloat(document.getElementById('side3').value); 
+    if(side1 === side2 && side2 === side3){
     alert("this is an Equilateral");
+    window.location.href="equilateral.html";
     }
-    else if(side1 === side2 || side1 === side3 || side2 === side3){
+    else if((side1 === side2) || (side1 === side3) || (side2 === side3)){
     alert("Isosceles");
+    window.location.href="isosceles.html";
     }
     else if((side1 + side2) <= side3 || (side1 + side3) <= side2 || (side2 + side3) <= side1) {
     alert("Not a definite triangle");
     }
     else{
     alert("Scalene");
+    window.location.href="scalene.html";
     }
     }
